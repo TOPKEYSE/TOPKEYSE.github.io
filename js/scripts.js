5debdd868fed2fd42d3a882a2f1d7bdd80f35332
TOPKEYSE <71188279+TOPKEYSE@users.noreply.github.com>
2020-09-12 21:07:40 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$('.keys_scene').parallax({
+		scalarX: 3,
+		scalarY: 9,
+		invertX: false,
+		invertY: false
+	});
+	$('#header_scene_back').parallax({
+		scalarX: 3,
+		scalarY: 9
+	});
+	$('#footer_scene').parallax({
+		scalarX: 3,
+		scalarY: 9
+	});
+	$('#footer_scene_back').parallax({
+		scalarX: 3,
+		scalarY: 9,
+		invertX: false,
+		invertY: false
+	});
+});
